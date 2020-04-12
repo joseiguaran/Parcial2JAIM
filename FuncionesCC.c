f2c83949c3119bb9352d34a4fa7927ea3a07ab95
joseiguaran <60902629+joseiguaran@users.noreply.github.com>
2020-04-12 18:39:56 -0500

    Update FuncionesCC.c

--- FuncionesCC.c
+++ FuncionesCC.c
@@ -40,7 +40,7 @@ void anadirUsuario(local_t matriz[][5], int tamano[])
     printf("Numero del Local");
     scanf("%d", &numLocalxPiso);
     
-    if (matriz[pisoLocal-1][numLocalxPiso-1].anioApertura=0)
+    if (matriz[pisoLocal-1][numLocalxPiso-1].anioApertura=)
     {
 	    
 	printf("Anio de Apertura ");
