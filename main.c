5a58a730d32c756fa0b116816cf759c89c1bd3e2
joseiguaran <60902629+joseiguaran@users.noreply.github.com>
2020-04-12 13:10:03 -0500

    Creacion de Menu

--- main.c
+++ main.c
@@ -3,5 +3,44 @@
 
 int main(){
 	
-	return 0;
+   local_t matriz[12][5];
+    int tamano[12] = {};
+    int opcion=0;
+    do{
+        opcion = menu();
+        
+        switch (opcion)
+        {
+        case 1:
+            disponibilidadLocal(matriz, tamano);
+            break;
+
+        case 2:
+            anadirLocal(matriz, tamano);
+            break;
+
+        case 3:
+            int aniosApertura;    
+            aniosApertura=sumarAnios(matriz, tamano);
+            printf("El total de Anios de Apertura para locales del piso es:" %d, aniosApertura);
+            break;
+
+        case 4:
+            buscarOficinas(matriz, tamano);
+            break;
+	
+	case 5:
+            cambiarNombre(matriz, tamano);
+            break;
+			
+	case 6:
+            vaciarLocal(matriz, tamano);
+            break;
+			
+        default:
+            printf("Opcion no Disponible\n");
+            break;
+        }
+    } while (option != 0);
 }
+return 0;
