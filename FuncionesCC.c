c408e8513d2d3a6c5f6e0288c171da819ca5aa48
joseiguaran <60902629+joseiguaran@users.noreply.github.com>
2020-04-12 16:41:17 -0500

    Update FuncionesCC.c

--- FuncionesCC.c
+++ FuncionesCC.c
@@ -63,7 +63,7 @@ void anadirUsuario(local_t matriz[][5], int tamano[])
 		}
    
         local_t l;
-        strcpy(p.nombreLocal, nombre);
+        strcpy(l.nombreLocal, nombre);
         l.pisoLocal=pisoLocal;
         l.numLocalxPiso=numLocalxPiso;
         l.tipoLocal=tip;
