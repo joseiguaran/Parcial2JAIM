dcd19c0241e70d3e83f7ac4a6d2f31d8b6bbc76e
joseiguaran <60902629+joseiguaran@users.noreply.github.com>
2020-04-12 13:29:19 -0500

    Anadir Menu

--- FuncionesCC.c
+++ FuncionesCC.c
@@ -1,14 +1,18 @@
 #include "FuncionesCC.h"
 
-/* Funcion de ejemplo que imprimiria la informacion de un local */
-void mostrarLocal(local_t ** centroComercial, int numPiso,
-		int numLocalxPiso, int numPisos, int numLocalesxPiso) {
-	
-	//Se valida si el numero ingresado esta en el rango
-	if(numPiso<=numLocalxPiso && numLocalxPiso <= numLocalesxPiso)
-	{
-		printf("Nombre local : %s", centroComercial[numPiso][numLocalxPiso].nombreLocal);
-		printf("Numero local : %d ", centroComercial[numPiso][numLocalxPiso].numeroLocal);
-	}
-	//TODO completarlo con los datos que le sirvan en su caso
+int menu()
+{
+    int opcion;
+    printf("BIENVENIDO AL SISTEMA DE LOCALES \n");
+    printf("Seleccione la opcion \n");
+    printf("1: Numero de locales disponibles\n");
+    printf("2: Anadir Local\n");
+    printf("3: Suma de Anios de Apertura por Piso\n");
+    printf("4: Locales por Tipo (Almacen u Oficina)\n");
+    printf("5: Cambiar Nombre del Almacen (Almacen u Oficina)\n");
+    printf("6: Vaciar Local (Almacen u Oficina)\n");
+    printf("0: Salir\n");
+    scanf("%d", &opcion);
+    return opcion;
+    
 }
